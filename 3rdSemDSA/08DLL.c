de487795c4655c4169ada77c0c53bf8f0406ae01
Shreekrishna Hegde <hegdeshreekrishna469@gmail.com>
2025-02-05 18:43:48 +0530

    Majot Changes

--- 3rdSemDSA/08DLL.c
+++ 3rdSemDSA/08DLL.c
@@ -41,3 +41,101 @@ Node* createNode(){
     newNode->next=NULL;
     return newNode;
 }
+
+void insertFirst(Node** head){
+    Node* newNode=createNode();
+    if(*head==NULL){
+        *head=newNode;
+    }
+    else{
+        newNode->next=*head;
+        (*head)->prev=newNode;
+        *head=newNode;
+    }
+}
+void insertLast(Node** head){
+    Node* newNode=createNode();
+    if(*head==NULL){
+        *head=newNode;
+    }
+    else{
+        Node* temp=*head;
+        while(temp->next!=NULL){
+            temp=temp->next;
+        }
+        temp->next=newNode;
+        newNode->prev=temp;
+    }
+}
+void deleteFirst(Node** head){
+    if(*head==NULL){
+        printf("List is Empty\n");
+    }
+    else if((*head)->next==NULL){
+        free(*head);
+        *head=NULL;
+    }
+    else{
+        Node* temp=*head;
+        *head=(*head)->next;
+        free(temp);
+        (*head)->prev=NULL;
+    }
+}
+void deleteLast(Node** head){
+    if(*head==NULL){
+        printf("List is Empty\n");
+    }
+    else if((*head)->next==NULL){
+        free(*head);
+        *head=NULL;
+    }
+    else{
+        Node* temp=*head;
+        while(temp->next!=NULL){
+            temp=temp->next;
+        }
+        temp->prev->next=NULL;
+        free(temp);
+    }
+}
+void display(Node* head){
+    int count=0;
+    if(head==NULL){
+        printf("List is Empty\n");
+    }
+    else{
+        Node* temp=head;
+        while(temp!=NULL){
+            printf("\nSSN:%d\nName:%s\nDept:%s\nDesignation:%s\nSal:%d\nPhNo:%s\n",temp->ssn,temp->name,temp->dept,temp->designation,temp->sal,temp->phNo);
+            temp=temp->next;
+            count++;
+        }
+        printf("Number of nodes:%d\n",count);
+    }
+}
+
+void main(){
+    Node* head=NULL;
+    int choice;
+    printf("\n1.Insert First\n2.Insert Last\n3.Delete First\n4.Delete Last\n5.Display\n6.Exit");
+    do{
+        printf("\nEnter your choice: ");
+        scanf("%d",&choice);
+        switch(choice){
+            case 1: insertFirst(&head);
+                    break;
+            case 2: insertLast(&head);
+                    break;
+            case 3: deleteFirst(&head);
+                    break;
+            case 4: deleteLast(&head);
+                    break;
+            case 5: display(head);
+                    break;
+            case 6: exit(0);
+            default: printf("Invalid Choice\n");
+        }
+    }
+    while(choice!=6);
+}
