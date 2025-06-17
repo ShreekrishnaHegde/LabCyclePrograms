22fa619077c4f716fba6df370c7a1e06dc39c4cd
Shreekrishna Hegde <hegdeshreekrishna469@gmail.com>
2025-06-17 19:28:11 +0530

    msg

--- 4thSemAlgo/12PrimsAlgo.cpp
+++ 4thSemAlgo/12PrimsAlgo.cpp
@@ -63,3 +63,16 @@ int function(int v,vector<pair<int,int>> adj[]){
     }
     return sum;
 }
+
+/*
+------------------------------------------------Algorithm------------------------------------------
+Step 1: Determine an arbitrary vertex as the starting vertex of the MST. We pick 0 in the below diagram.
+Step 2: Follow steps 3 to 5 till there are vertices that are not included in the MST (known as fringe vertex).
+Step 3: Find edges connecting any tree vertex with the fringe vertices.
+Step 4: Find the minimum among these edges.
+Step 5: Add the chosen edge to the MST. Since we consider only the edges that connect fringe vertices with the rest, we never get a cycle.
+Step 6: Return the MST and exit
+
+TC: O(E log V)
+SC: O(V + E)
+*/
